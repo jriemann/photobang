99fb3fe7666ae81f167b62f108ad825bb68ba1ba
davidhandesigns <davidhandeveloper@gmail.com>
2016-01-23 11:48:17 -0500

    Added PhotObject

---
+++ androidApp/PhoneBang/app/src/main/java/com/davidhan/photobang/backend/base/PhotoObject.java
@@ -0,0 +1,11 @@
+package com.davidhan.photobang.backend.base;
+
+/**
+ * name: PhotoObject
+ * desc:
+ * date: 2016-01-23
+ * author: david
+ * Copyright (c) 2016 David Han
+ **/
+public class PhotoObject {
+}
