1a33b079208d27977e810467821c12fd2f9ab1b2
shreymahendru <shreymahendru@gmail.com>
2016-01-24 01:00:40 -0500

    Worked on image processing

--- androidApp/PhoneBang/app/src/main/java/com/davidhan/photobang/frontend/processingscreen/activities/ProcessingActivity.java
+++ androidApp/PhoneBang/app/src/main/java/com/davidhan/photobang/frontend/processingscreen/activities/ProcessingActivity.java
@@ -23,10 +23,13 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.photo.Photo;
 
 import com.davidhan.photobang.R;
+import com.davidhan.photobang.backend.base.PhotoObject;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -57,10 +60,23 @@ public class ProcessingActivity extends Activity{
         ButterKnife.bind(this);
         Log.i(TAG, "Init with");
         double before = System.currentTimeMillis();
-        getImagesFromSd();
-        Log.i(TAG, "Time elapsed: " + (System.currentTimeMillis()-before)+" ms.");
+        //getImagesFromSd();
+
+        ArrayList<File> test_files = new ArrayList<File>()
+        {{
+                add(new File("blurry_3.jpg"));
+                add(new File("blurry_5.jpg"));
+                add(new File("SCREENSHOT_face_2.jpg"));
+                add(new File("test.jpg"));
+                add(new File("test2.jpg"));
+                add(new File("black.jpg"));
+            }};
+        ArrayList<File> blurry_files = getBadPhotos(test_files, true); // Change this later
+        ArrayList<Long> file_sizes = getTotalFileSize(test_files);
+        Log.i(TAG, "Time elapsed: " + (System.currentTimeMillis() - before) + " ms.");
+
     }
-    private void getImagesFromSd()
+    /*private void getImagesFromSd()
     {
         String ExternalStorageDirectoryPath = Environment.getExternalStorageDirectory().getAbsolutePath();
         String targetpath = ExternalStorageDirectoryPath + "/test/";
@@ -70,13 +86,80 @@ public class ProcessingActivity extends Activity{
         for (File file :files)
         {
             boolean result = initView(file.getAbsolutePath());
-            Log.i(TAG, "Pictuer Results ="+ result );
+            Log.i(TAG, "Picture Results ="+ result );
         }
+    }*/
+
+    private ArrayList<File> getBadPhotos(ArrayList<File> all_files, boolean detectScreenshots) {
+        ArrayList<File> bad_files = new ArrayList<File>();
+        for (File file :all_files)
+        {
+            if(isBlurry(file) || (detectScreenshots && isScreenshot(file))) {
+                bad_files.add(file);
+                Log.i(TAG, file + " is blurry or a screenshot");
+            }
+        }
+
+        return bad_files;
+    }
+
+    private int deletePhotos(ArrayList<PhotoObject> photos) {
+        String filename;
+        File file;
+        int count = 0;
+        for (PhotoObject photo :photos)
+        {
+            filename = photo.getFile().getAbsolutePath();
+            file = new File(filename);
+            if (!file.delete()){
+                count += 1;
+                Log.i(TAG, "File deletion failed on " + filename);
+                return -1;
+            }
+        }
+        Log.i(TAG, "File deletion completed successfully! Deleted " + count + " files.");
+        return count;
+    }
+
+    /**
+     * Take a list of file objects, and return a list of equal size
+     * containing the corresponding file sizes.
+     * @param file_list
+     * @return
+     */
+    private ArrayList<Long> getTotalFileSize(ArrayList<File> file_list) {
+        ArrayList<Long> file_sizes = new ArrayList<Long>();
+        long size;
+        String name;
+        for (File file :file_list) {
+            if (file.exists()) {
+                size = ((long) file.length());
+                name = file.getAbsolutePath();
+                Log.i(TAG, "File name is: " + name + " megabytes");
+                file_sizes.add(size);
+                Log.i(TAG, "File size is: " + size + " megabytes");
+            } else {
+                Log.i(TAG, file.getAbsolutePath() + " was not found");
+            }
+        }
+        return file_sizes;
+
+    }
+    private boolean isScreenshot(File file) {
+        String filename = file.getName();
+        filename=filename.toUpperCase();
+        if (filename.length()< 9 )
+        {
+            Log.i(TAG, filename + "is a screenshot: false.");
+            return false;
+        }
+        String slice = filename.substring(0, 10);
+        Log.i(TAG, filename + "is a screenshot: " + slice.equals("SCREENSHOT") + ". Slice is: " + slice);
+        return (slice.equals("SCREENSHOT"));
     }
 
 
-
-    private boolean initView(String Path) {
+    private boolean isBlurry(File file) {
         //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
         AssetManager assetManager = getAssets();
@@ -87,36 +170,36 @@ public class ProcessingActivity extends Activity{
 //        }
 
         try {
-            is = assetManager.open("test2.jpg");
+            is = assetManager.open(file.getName()) ;//.getName());
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-        Bitmap bitmap = BitmapFactory.decodeFile(Path);
+        Bitmap bitmap = BitmapFactory.decodeStream(is);
         Log.i(TAG, "Got the bitmap");
 
         imgMAT = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC1);
         //imgMAT = new Mat();
 
-        Log.i(TAG, "Mat declared");
+        //Log.i(TAG, "Mat declared");
         int width = bitmap.getWidth();
         int height = bitmap.getHeight();
 
-        Log.i(TAG, "width = " + bitmap.getWidth() + " height = " + bitmap.getHeight());
+        //Log.i(TAG, "width = " + bitmap.getWidth() + " height = " + bitmap.getHeight());
         Utils.bitmapToMat(bitmap, imgMAT);
 
         greyMAT = new Mat(height, width, CvType.CV_8UC1);
         Imgproc.cvtColor(imgMAT, greyMAT, Imgproc.COLOR_RGBA2GRAY);
-        Log.i(TAG, "Converted to grey.");
+        //Log.i(TAG, "Converted to grey.");
 
         //int sharpness = getSharpness(greyMAT, width, height);
         //Log.i(TAG, "sharpess: " + sharpness);
         Imgproc.Laplacian(greyMAT, greyMAT, greyMAT.depth());
-        Log.i(TAG, "Converted to Laplacian.");
+        //Log.i(TAG, "Converted to Laplacian.");
 
         convertScaleAbs(greyMAT, greyMAT);
-        Log.i(TAG, "Converted to scale abs.");
+        //Log.i(TAG, "Converted to scale abs.");
 
 
         BitmapFactory.Options opt = new BitmapFactory.Options();
@@ -181,8 +264,8 @@ public class ProcessingActivity extends Activity{
         }
 
         int soglia = -6118750;
-        Log.i(TAG, "WHAAAT: " + (maxLap < soglia) );
-        Log.i(TAG, "maxLap: " + maxLap);
+        //Log.i(TAG, "WHAAAT: " + (maxLap < soglia) );
+        //Log.i(TAG, "maxLap: " + maxLap);
         if (maxLap < soglia || maxLap == soglia) {
             Log.i(TAG, "blur image");
             return true;
@@ -200,26 +283,5 @@ public class ProcessingActivity extends Activity{
         //    Log.i(TAG, "Image is blurry: " + maxLap );
         //}
     }
-
-    /*public int getSharpness(Mat bitmap, int width, int height)
-    {
-        // assumes that your image is already in planner yuv or 8 bit greyscale
-        IplImage* in = cvCreateImage(cvSize(width,height),IPL_DEPTH_8U,1);
-        IplImage* out = cvCreateImage(cvSize(width,height),IPL_DEPTH_16S,1);
-        memcpy(in->imageData,data,width*height);
-
-        // aperture size of 1 corresponds to the correct matrix
-        cvLaplace(in, out, 1);
-
-        short maxLap = -32767;
-        short* imgData = (short*)out->imageData;
-        for(int i =0;i<(out->imageSize/2);i++)
-        {
-            if(imgData[i] > maxLap) maxLap = imgData[i];
-        }
-
-        cvReleaseImage(&in);
-        cvReleaseImage(&out);
-        return maxLap;
-    } */
+    
 }
