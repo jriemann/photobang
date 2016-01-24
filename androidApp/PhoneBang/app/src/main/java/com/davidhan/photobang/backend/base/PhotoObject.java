db95607e4f974435cc68cf5aa09688bb13b167f2
davidhandesigns <davidhandeveloper@gmail.com>
2016-01-23 22:11:49 -0500

    photoobject

--- androidApp/PhoneBang/app/src/main/java/com/davidhan/photobang/backend/base/PhotoObject.java
+++ androidApp/PhoneBang/app/src/main/java/com/davidhan/photobang/backend/base/PhotoObject.java
@@ -1,5 +1,11 @@
 package com.davidhan.photobang.backend.base;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+
+import java.io.File;
+
 /**
  * name: PhotoObject
  * desc:
@@ -8,4 +14,41 @@ package com.davidhan.photobang.backend.base;
  * Copyright (c) 2016 David Han
  **/
 public class PhotoObject {
+
+    /**
+     * Path of the file
+     */
+    private File file;
+    private boolean selected;
+    private Bitmap thumbnail;
+    private Bitmap fullImage;
+    public PhotoObject(File file){
+        this.file = file;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public Bitmap getThumbnail() {
+        if(thumbnail == null){
+            thumbnail = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(file.getPath()), 128, 128);
+        }
+        return thumbnail;
+    }
+
+    public Bitmap getFullImage() {
+        if(fullImage == null){
+            fullImage = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(file.getPath()), 1080 , 1920);
+        }
+        return fullImage;
+    }
 }
